9dd6d806850de34bc1068ace277eccb2465032cc
AG989789 <126360569+AG989789@users.noreply.github.com>
2024-08-30 12:03:20 -0400

    Update Bulbasaur.java

--- ex2/src/main/java/Bulbasaur.java
+++ ex2/src/main/java/Bulbasaur.java
@@ -1,60 +1,54 @@
 public class Bulbasaur {
-    private int id;
-    private int level;
 
-    // Constructor
-    public Bulbasaur() {
-        id = 1;
-        level = 1;
-    }
-
-    // setLevel method
-    public void setLevel(int lv) {
-        this.level = lv;
-        if(level >= 16 && level =< 31
-            this.id = 2;
-        if(level >= 32
-            this.od = 3;
-
-    }
-
-    // getLevel method
-    public int getLevel() {
-        return level;
-    }
-
-    // getName method
-    public String getName() {
-        if(id == 1)
-            return "Bulbasaur";
-        if(id == 2)
-            return "Ivysaur";
-        if(id == 3)
-            return "Venusaur";
-
-    }
-
-    // getID method
-    public int getID() {
-        return id;
-    }
-
-    // toString method
-    @Override
-    public String toString() {
-        return "Level: " + getLevel() + ", ID " + id;
-    }
-
-    // equals method
-    @Override
-    public boolean equals(Object obj) {
-        if(this.getLevel() == other.getLevel && this.getID() == other.getID)
+   private int id;
+   private int level;
+    
+   public Bulbasaur() {
+      id = 1;
+      level = 1;
+   }
+   public Bulbasaur(int id, int level) {
+      this.id = id;
+      this.level = level;
+   }
+   
+   public void setLevel(int lv) {
+      this.level = lv;
+      if(level >= 16 && level <= 31)
+         this.id = 2;
+      if(level >= 32)
+         this.id = 3;
+   }
+   
+   public int getLevel() {
+      return level;
+   }
+   
+   public String getName() {
+      if(id == 1)
+         return "Bulbasaur";
+      if(id == 2)
+         return "Ivysaur";
+      if(id == 3)
+         return "Venusaur";
+      return "";
+   }
+   
+   public String toString() {
+      return getName() + ": Level: " + getLevel();
+   }
+   
+   public boolean equals(Bulbasaur other) {
+      if(this.getLevel() == other.getLevel() && this.getID() == other.getID())
          return true;
       return false;
-    }
-
-    // copy method
-    public Bulbasaur copy() {
-        return new Bulbasaur(this.id, this.level);
-    }
+   }
+   
+   public Bulbasaur copy() {
+      return new Bulbasaur(this.id, this.level);
+   }
+ 
+   public int getID() {
+      return id;
+   }
 }
